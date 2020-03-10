297b5a8ba4f3bf54b952019d160aa6f40143202d
jiatian <jiatian@nod-labs.com>
2020-03-09 18:01:25 -0700

    add undistort and bilateral filter

--- undistort.py
+++ undistort.py
@@ -9,8 +9,8 @@ class Undistort():
         self.output_dir = output_dir
         os.makedirs(self.output_dir, exist_ok=True)
 
-        self.K = np.array([[583.780379265, 0.0, 651.918915441], [0.0, 581.614977546, 393.602999407], [0.0, 0.0, 1.0]])
-        self.D = np.array([[0.000455566535289], [-0.0107723719394], [0.0126745953652], [-0.00432266278961]])
+        self.K = np.array([[286.92994997677204, 0.0, 324.67953012335556], [0.0, 286.4856509324264, 197.07953340996804], [0.0, 0.0, 1.0]])
+        self.D = np.array([[-0.00022113678021126206], [-0.0011227150767492507], [0.006647502459197323], [-0.0031347580009617264]])
 
         # self.K = np.array([[501.00957821, 0.0, 713.40403809], [0.0, 499.15119612, 378.33847811], [0.0, 0.0, 1.0]])
         # self.D = np.array([[0.0], [0.0], [0.0], [0.0]])
@@ -25,7 +25,7 @@ class Undistort():
         h, w = img.shape[:2]
         DIM = (w, h)
 
-        dim1 = (1280, 800)
+        dim1 = (640, 400)
         dim2 = dim1
         dim3 = dim1
 
@@ -36,8 +36,7 @@ class Undistort():
 
         map1, map2 = cv2.fisheye.initUndistortRectifyMap(scaled_K, self.D, np.eye(3), new_K, dim3, cv2.CV_16SC2)
         dst = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
-        import pdb; pdb.set_trace()
-        return dst
+
         # newcameramtx, roi=cv2.getOptimalNewCameraMatrix(self.K, self.D,(w,h),1,(w,h))
 
         # undistort
@@ -47,11 +46,11 @@ class Undistort():
         # x,y,w,h = roi
         # dst = dst[y:y+h, x:x+w]
 
-        # import pdb; pdb.set_trace()
-
-        # cv2.imshow('orig', img)
-        # cv2.imshow('dst', dst)
+        cv2.imshow('orig', img)
+        cv2.imshow('dst', dst)
         # cv2.waitKey(0)
+
+        return dst
 
     def undistort(self):
         img_list = os.listdir(self.input_dir)
@@ -70,7 +69,7 @@ class Undistort():
             print(img)
 
 if __name__ == "__main__":
-    input_dir = '/media/jiatian/data/LH_TH_MM_3/michigan/ios/imageDump0/'
-    output_dir = '/media/jiatian/data_resize/LH_TH_MM_3/michigan/ios/imageDump3/'
+    input_dir = '/home/jiatian/dataset/nod_device/Demo_Record300_1/nodvi/device/data/images0/'
+    output_dir = '/home/jiatian/dataset/nod_device/Demo_Record300_1/nodvi/device/data/images0_undistorted/'
     instance = Undistort(input_dir, output_dir)
     instance.undistort()
